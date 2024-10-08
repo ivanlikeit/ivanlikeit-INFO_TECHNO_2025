5538b3b99d8de66895f285a5d82f8a4ffc63a48c
ivanlikeit <ivankanakov2d@gmail.com>
2024-10-08 23:07:28 +0300

    ad

---
+++ main.py
@@ -0,0 +1,11 @@
+import itertools as iter
+enumeration = list(iter.permutations('0123456789', r = 6))
+numbers = list('13579')
+count = 0
+for x in enumeration:
+    x = ''.join(x)
+    if int(x) % 5 == 0:
+        for i in range(6): x = x.replace(x[i],'*') if x[i] in numbers else x.replace(x[i],'@')
+        if '@@' not in x and '**' not in x:
+            count +=1
+print(count)
