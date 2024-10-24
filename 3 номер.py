5ec160a32b4514c601ddb802ffb70183d411782c
ivanlikeit <ivankanakov2d@gmail.com>
2024-10-24 19:27:18 +0300

    ad

--- 3 номер.py
+++ 3 номер.py
@@ -4,7 +4,7 @@ numbers = list('13579')
 count = 0
 for x in enumeration:
     x = ''.join(x)
-    if int(x) % 5 == 0:
+    if int(x) % 5 == 0 and x[0] != '0':
         for i in range(6): x = x.replace(x[i],'*') if x[i] in numbers else x.replace(x[i],'@')
         if '@@' not in x and '**' not in x:
             count +=1
